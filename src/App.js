859dfb13586a23bb973933674c14ee270c9968c9
yogesh.ktm <yogesh.ktm@gmail.com>
2021-07-08 12:36:13 +0530

    warnings fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-import TodoContainer from './container/TodoContainer';
+import TodoContainer from "./container/TodoContainer";
 function App() {
   return (
     <div className="App">
-        <h1 className="logo">Todo List</h1>
-        <TodoContainer></TodoContainer>
+      <h1 className="logo">Todo List</h1>
+      <TodoContainer></TodoContainer>
     </div>
   );
 }
